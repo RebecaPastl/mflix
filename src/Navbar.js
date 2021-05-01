f4e7fb053400ef92d923479eea823d653db52f54
RebecaPastl <rebeca.pastl@hotmail.com>
2021-04-30 19:27:43 -0700

    feat(navlink): #29 add style to nav links

--- src/Navbar.js
+++ src/Navbar.js
@@ -20,17 +20,17 @@ class Navbar extends React.Component {
                 <Bar collapseOnSelect className="shadow bg-secondary" variant='dark' expand="lg" fixed='top' >
                     <Bar.Brand>
                         <NavLink className='text-white mr-3 text-decoration-none' to='/'>
-                            <BsFilm /> MFLIX
+                            <BsFilm /> <span className='h1 small'>MFLIX</span>
                         </NavLink>
                     </Bar.Brand>
                     <Bar.Toggle aria-controls="responsive-navbar-nav" />
                     <Bar.Collapse id="responsive-navbar-nav">
                         <Nav className="mr-auto">
-                            <NavLink className='hover-effect btn btn-secondary mr-3 p-2 text-decoration-none' to='/all'>
-                                All Movies
+                            <NavLink className='hover-effect btn btn-secondary mr-3 mt-2 p-2 h2 text-white text-decoration-none' to='/all'>
+                                ALL MOVIES
                             </NavLink>
-                            <NavLink className='hover-effect btn btn-secondary mr-3 p-2 text-decoration-none' to='/genre'>
-                                Movies by Genre
+                            <NavLink className='hover-effect btn btn-secondary mr-3 mt-2 p-2 h2 text-white text-decoration-none' to='/genre'>
+                                MOVIES BY GENRE
                             </NavLink>
                         </Nav>
                     </Bar.Collapse>
