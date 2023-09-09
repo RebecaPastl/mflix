7ecdc1d86bbd29454a274855fae453f7bd8f434b
Rebeca Pastl <rebeca.pastl@gmail.com>
2023-09-09 14:20:04 -0700

    refactor header component

--- src/Header.js
+++ src/Header.js
@@ -1,34 +1,24 @@
-//imports
-//when in the client side, use import syntax
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 
-class Header extends React.Component {
-
-    render() {
-        
-        //render header
-        return <>
-            
-            <header className='mt-5'>
-                <Card className="bg-dark text-white ">
-                    <Card.Img src="./images/banner.png" alt="MFLIX Banner" />
-                    <Card.ImgOverlay>
-                        <Card.Title>
-                            <h1 className='header-h1 text-dark opacity-4'>MFLIX</h1>
-                        </Card.Title>
-                        </Card.ImgOverlay>
-                        <Card.ImgOverlay className='d-flex flex-column justify-content-end'>
-                        <Card.Text>
-                            <p className='artist mb-0 small text-right'>Photo by Skitterphoto from <a className='text-muted' href="https://www.pexels.com/photo/abstract-analog-art-camera-390089/">Pexels</a></p>
-                        </Card.Text>
+function Header() {
+    return <>
+        <header className='mt-5'>
+            <Card className="bg-dark text-white ">
+                <Card.Img src="./images/banner.png" alt="MFLIX Banner" />
+                <Card.ImgOverlay>
+                    <Card.Title>
+                        <h1 className='header-h1 text-dark opacity-4'>MFLIX</h1>
+                    </Card.Title>
                     </Card.ImgOverlay>
-                </Card>
-            </header>
-            
-        </>;
-    }
-
+                    <Card.ImgOverlay className='d-flex flex-column justify-content-end'>
+                    <Card.Text>
+                        <p className='artist mb-0 small text-right'>Photo by Skitterphoto from <a className='text-muted' href="https://www.pexels.com/photo/abstract-analog-art-camera-390089/">Pexels</a></p>
+                    </Card.Text>
+                </Card.ImgOverlay>
+            </Card>
+        </header>
+    </>;
 }
 
 export default Header;
